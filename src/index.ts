cba42ad57beeeb4dcd3af87a3e1af21ba42e8ca4
tikaram <tikaramthapamgr@gmail.com>
2026-06-29 17:03:12 +1000

    Server url added on env file.

--- src/index.ts
+++ src/index.ts
@@ -12,5 +12,5 @@ app.use(router);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at ${process.env.SERVER_URL}:${port}`);
 });
